7343e511cd526de49386736b818b37865d5e9a62
sallyrabs <69092414+sallyrabs@users.noreply.github.com>
2022-03-01 00:20:38 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function   onblb(){
+    document.getElementById('bulbimage').src ='./assets/images/onblb.gif.jfif'
+    }
+    function offblb() {
+      document.getElementById('bulbimage').src ='./assets/images/offblb.gif.jfif'  
+    }
